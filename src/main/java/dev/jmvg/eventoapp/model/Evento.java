10464ddff26b8218e685e8db3adcf25295e23e0f
Lenk <jmvg.comp@gmail.com>
2019-07-22 23:50:03 -0300

    Commit inicial da aplicação

---
+++ src/main/java/dev/jmvg/eventoapp/model/Evento.java
@@ -0,0 +1,37 @@
+package dev.jmvg.eventoapp.model;
+
+public class Evento {
+    private String nome, local, data, horario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
