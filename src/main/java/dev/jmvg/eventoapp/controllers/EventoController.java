b2839ecfe9f5e373b7a1681d394d4072e10c67d0
Lenk <jmvg.comp@gmail.com>
2019-07-25 01:44:28 -0300

    Listando eventos

--- src/main/java/dev/jmvg/eventoapp/controllers/EventoController.java
+++ src/main/java/dev/jmvg/eventoapp/controllers/EventoController.java
@@ -1,13 +1,38 @@
 package dev.jmvg.eventoapp.controllers;
 
+import dev.jmvg.eventoapp.model.Evento;
+import dev.jmvg.eventoapp.repository.EventoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
 
-    @RequestMapping("/cadastrarEvento")
+    private final EventoRepository eventoRepository;
+
+    public EventoController(EventoRepository eventoRepository) {
+        this.eventoRepository = eventoRepository;
+    }
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
     public String form(){
         return "evento/formEvento";
     }
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+    public String form(Evento evento){
+        eventoRepository.save(evento);
+        return "redirect:/cadastrarEvento";
+    }
+
+    @RequestMapping(value = "/eventos")
+    public ModelAndView listaEventos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = eventoRepository.findAll();
+        mv.addObject("eventos", eventos);
+        return mv;
+    }
+
 }
---
+++ src/main/java/dev/jmvg/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,7 @@
+package dev.jmvg.eventoapp.repository;
+
+import dev.jmvg.eventoapp.model.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+ public interface EventoRepository extends CrudRepository<Evento, Long> {
+}
